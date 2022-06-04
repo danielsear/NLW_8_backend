7df9eab78d99b1403586219eb2cb9be7cfee596a
Danielsear <danielandradeseara@hotmail.com>
2022-06-04 18:13:11 -0300

    created route feedbacks

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import express from 'express'
+import { Prisma } from './prisma'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World!' })
+app.use(express.json())
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body
+  const feedback = await Prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+  return res.status(201).json({ data: feedback })
 })
 
 app.listen(3333, () => {
